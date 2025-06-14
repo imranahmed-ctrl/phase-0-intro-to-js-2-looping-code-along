1194cc240f5225183d1bd5e9e375803aa469a617
imran <otcimran16@gmail.com>
2025-06-14 14:11:45 +0300

    commits

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Export for test files if needed
+module.exports = { writeCards, countDown };
